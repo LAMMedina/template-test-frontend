9a7e6827c126796add50aab1dad80a4423c7f2a3
Luis Mujica <luismujicamedina@gmail.com>
2024-12-05 17:34:21 -0500

    Capa GeoJSON de departamentos cargada

--- pages/Viewer.jsx
+++ pages/Viewer.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
-import Map from 'ol/Map';
-import View from 'ol/View';
+import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import GeoJSON from 'ol/format/GeoJSON';
 
 const Viewer = () => {
   useEffect(() => {
@@ -14,10 +16,22 @@ const Viewer = () => {
         }),
       ],
       view: new View({
-        center: [-8341494.78, -1780719.66], // Coordenadas de Perú en EPSG:3857
-        zoom: 6,
+        center: [-8246936.494308056, -1119428.8251098266], 
+        zoom: 7,
+        maxZoom: 50,
+        minZoom: 5,
       }),
     });
+
+    // Añadir la capa de departamentos
+    const departamentosLayer = new VectorLayer({
+      source: new VectorSource({
+        url: '/data/output_departamentos.geojson',
+        format: new GeoJSON(),
+      }),
+    });
+
+    map.addLayer(departamentosLayer);
 
     return () => map.setTarget(null);
   }, []);
