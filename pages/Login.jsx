5984523543dba6da53ad1e57836370e4eb1c2716
Luis Mujica <luismujicamedina@gmail.com>
2024-12-05 21:27:54 -0500

    Validacion de formulario

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, TextField, Button, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleLogin = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
+    if (!email || !password) {
+      alert('Por favor, complete todos los campos.');
+      return;
+    }
     navigate('/visor');
   };
 
@@ -20,11 +27,29 @@ const Login = () => {
       <Typography variant="h4" mb={3}>
         Iniciar Sesión
       </Typography>
-      <TextField label="Correo Electrónico" fullWidth margin="normal" />
-      <TextField label="Contraseña" type="password" fullWidth margin="normal" />
-      <Button variant="contained" onClick={handleLogin} sx={{ mt: 2 }}>
-        Iniciar
-      </Button>
+      <form onSubmit={handleLogin}>
+        <TextField 
+          label="Correo Electrónico" 
+          fullWidth
+          type="email"
+          margin="normal" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+          required
+        />
+        <TextField 
+          label="Contraseña" 
+          type="password" 
+          fullWidth 
+          margin="normal" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+          required
+        />
+        <Button type="submit" variant="contained" sx={{ mt: 2 }}>
+          Iniciar
+        </Button>
+      </form>
     </Box>
   );
 };
